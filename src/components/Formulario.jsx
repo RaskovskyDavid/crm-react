81c95ae7ac0af71fe224724eea7f9ecb5f5bd238
david <davidtucu@hotmail.com>
2022-09-03 15:44:29 -0300

    configurar valores iniciales en un formularios con formik

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,56 @@
 import React from 'react'
 import { Formik, Form, Field } from 'formik'
 const Formulario = () => {
+    const handleSubmit = async (valores) => {
+        // try {
+        //     let respuesta 
+        //     if(cliente.id) {
+        //         // Editando un registro
+        //         const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`
+        //         respuesta = await fetch(url, {
+        //             method: 'PUT',
+        //             body: JSON.stringify(valores),
+        //             headers: {
+        //                 'Content-Type': 'application/json'
+        //             }
+        //         })
+
+        //     } else {
+        //         // Nuevo Registro
+        //         const url = import.meta.env.VITE_API_URL
+        //         respuesta = await fetch(url, {
+        //             method: 'POST',
+        //             body: JSON.stringify(valores),
+        //             headers: {
+        //                 'Content-Type': 'application/json'
+        //             }
+        //         })
+        //     }
+
+        //     await respuesta.json()
+        //     navigate('/clientes')
+        // } catch (error) {
+        //     console.log(error)
+        // }
+    }
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md 
             md:w-3/4 mx-auto text-left">
                 <h1 className="text-gray-600 font-bold text-xl uppercase text-center">Editar Cliente</h1>
         <Formik
-
+            initialValues={{
+                nombre: "",
+                empresa:  "",
+                email: "",
+                telefono:  "",
+                notas: "",
+            }}
+            onSubmit={ async (values, {resetForm}) => {
+                await handleSubmit(values)
+                resetForm()
+            }}
         >
+            {() => (
             <Form className="mt-10">
                 <div className="mb-4">
                 <label className="text-gray-800"
@@ -80,6 +123,7 @@ const Formulario = () => {
                         />    
                         </div>        
             </Form>
+             )}
         </Formik>
       
     </div>
